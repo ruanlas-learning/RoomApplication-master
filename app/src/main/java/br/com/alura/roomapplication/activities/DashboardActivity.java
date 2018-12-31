9f11541897c62439a2f079d0b8093e7f2e02515e
Ruan Silva <ruanluiz.as@gmail.com>
2018-12-31 06:14:58 -0200

    first commit

---
+++ app/src/main/java/br/com/alura/roomapplication/activities/DashboardActivity.java
@@ -0,0 +1,15 @@
+package br.com.alura.roomapplication.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.alura.roomapplication.R;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
